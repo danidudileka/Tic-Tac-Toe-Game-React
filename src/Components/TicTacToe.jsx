201dffe50481b4a837af0f6aed5f72f27140a29d
danidudileka <danidudilekaperera@gmail.com>
2024-03-18 11:55:36 +0530

    Created box layout

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import './TicTacToe.css'
+import React from "react";
+import "./TicTacToe.css";
 
 const TicTacToe = () => {
   return (
     <div className="container">
-        <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
-        <div className="bord">
-
+      <h1 className="title">
+        Tic Tac Toe Game In <span>React</span>
+      </h1>
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
         </div>
-        <button className="reset">Reset</button>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      </div>
+      <button className="reset">Reset</button>
     </div>
-  )
-}
+  );
+};
 
-export default TicTacToe
+export default TicTacToe;
