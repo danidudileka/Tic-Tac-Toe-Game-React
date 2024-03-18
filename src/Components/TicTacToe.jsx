af0483fef75347ac1ce83e260f4b7e9c5cbfe568
danidudileka <danidudilekaperera@gmail.com>
2024-03-18 13:48:58 +0530

    Logic changes

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -5,10 +5,24 @@ import Circle from "./assets/circle.png";
 import Cross from "./assets/cross.png";
 
 let data = ["", "", "", "", "", "", "", "", ""];
+
 const TicTacToe = () => {
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState(false);
-  let titleRef = useRef(null)
+  let [title, setTitle] = useState(`Tic Tac Toe Game In React`);
+
+  let titleRef = useRef(null);
+  let box1 = useRef(null);
+  let box2 = useRef(null);
+  let box3 = useRef(null);
+  let box4 = useRef(null);
+  let box5 = useRef(null);
+  let box6 = useRef(null);
+  let box7 = useRef(null);
+  let box8 = useRef(null);
+  let box9 = useRef(null);
+
+  let boxArray = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 
   const toggle = (e, num) => {
     if (lock) {
@@ -38,46 +52,49 @@ const TicTacToe = () => {
     else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {won(data[6])}
   };
 
-    const won = (winner) => {
-      setLock(true);
-      if (winner === "x") {
-        titleRef.current.innerHTML = `Congratulations: <img src='${Cross}'> Wins`;
-      } else if (winner === "o") {
-        titleRef.current.innerHTML = `Congratulations: <img src='${Circle}'> Wins`;
-      }
-    };
-
-    const handleReset = () => {
-        setLock(false)
-        data = ["", "", "", "", "", "", "", "", ""]
-        titleRef.current.innerHTML = `Tic Tac Toe Game In <span>React</span></h1>`
+  const won = (winner) => {
+    setLock(true);
+    if (winner === "x") {
+      setTitle(`Congratulations: X Wins`);
+    } else if (winner === "o") {
+      setTitle(`Congratulations: O Wins`);
     }
+  };
+
+  const handleReset = () => {
+    setLock(false);
+    data = ["", "", "", "", "", "", "", "", ""];
+    setTitle("Tic Tac Toe Game In React");
+    boxArray.forEach((box) => {
+      box.current.innerHTML = "";
+    });
+  };
 
   return (
     <div className="container">
       <h1 className="title" ref={titleRef}>
-        Tic Tac Toe Game In <span>React</span>
+        {title}
       </h1>
       <div className="board">
         <div className="row1">
-          <div className="boxes" onClick={(e) => {toggle(e, 0)}}></div>
-          <div className="boxes" onClick={(e) => {toggle(e, 1)}}></div>
-          <div className="boxes" onClick={(e) => {toggle(e, 2)}}></div>
+          <div className="boxes" ref={box1} onClick={(e) => toggle(e, 0)}></div>
+          <div className="boxes" ref={box2} onClick={(e) => toggle(e, 1)}></div>
+          <div className="boxes" ref={box3} onClick={(e) => toggle(e, 2)}></div>
         </div>
-
         <div className="row2">
-          <div className="boxes" onClick={(e) => {toggle(e, 3)}}></div>
-          <div className="boxes" onClick={(e) => {toggle(e, 4)}}></div>
-          <div className="boxes" onClick={(e) => {toggle(e, 5)}}></div>
+          <div className="boxes" ref={box4} onClick={(e) => toggle(e, 3)}></div>
+          <div className="boxes" ref={box5} onClick={(e) => toggle(e, 4)}></div>
+          <div className="boxes" ref={box6} onClick={(e) => toggle(e, 5)}></div>
         </div>
-
         <div className="row3">
-          <div className="boxes" onClick={(e) => {toggle(e, 6)}}></div>
-          <div className="boxes" onClick={(e) => {toggle(e, 7)}}></div>
-          <div className="boxes" onClick={(e) => {toggle(e, 8)}}></div>
+          <div className="boxes" ref={box7} onClick={(e) => toggle(e, 6)}></div>
+          <div className="boxes" ref={box8} onClick={(e) => toggle(e, 7)}></div>
+          <div className="boxes" ref={box9} onClick={(e) => toggle(e, 8)}></div>
         </div>
       </div>
-      <button className="reset" onClick={handleReset}>Reset</button>
+      <button className="reset" onClick={handleReset}>
+        Reset
+      </button>
     </div>
   );
 };
